0486ff1308174b3776076c9ee5636c5958305401
Alex Langevin <alex_langevin@hotmail.ca>
2020-08-30 17:18:52 -0400

    Routes implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+import Bookmarks from './Bookmarks';
+import HomeFeed from './HomeFeed';
+import Notifications from './Notifications';
+import Profile from './Profile';
+import TweetDetails from './TweetDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <HomeFeed />
+        </Route>
+
+        <Route exact path='/notifications'>  
+          <Notifications />
+        </Route>
+
+        <Route exact path='/bookmarks'>
+          <Bookmarks />
+        </Route>
+
+        <Route exact path='/tweet/:tweetId'>
+          <TweetDetails />
+        </Route>
+
+        <Route exact path='/:profileId'>
+          <Profile />
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/TweetDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TweetDetails = () => {
+  return(
+    <div>
+      TweeetDetails
+    </div>
+  ) 
+}
+
+export default TweetDetails;
